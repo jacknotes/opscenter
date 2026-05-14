c8d8e26965adc0766d16950c63d88ecd6b8ce46a
李标 <jack.li@homsom.com>
2026-05-14 20:55:15 +0800

    优化全局布局，添加侧边栏收缩和扩展功能。

--- web/src/components/Layout.vue
+++ web/src/components/Layout.vue
@@ -1,45 +1,51 @@
 <template>
   <el-container style="height: 100vh">
-    <el-aside width="220px" style="background-color: #304156">
-      <div style="height: 60px; display: flex; align-items: center; justify-content: center; color: #fff; font-size: 18px; font-weight: bold;">
-        OpsCenter
+    <el-aside :width="isCollapse ? '64px' : '220px'" style="background-color: #304156;">
+      <div style="height: 60px; display: flex; align-items: center; justify-content: center; color: #fff; font-size: 18px; font-weight: bold; white-space: nowrap; overflow: hidden;">
+        {{ isCollapse ? 'OC' : 'OpsCenter' }}
       </div>
       <el-menu
         :default-active="route.path"
         router
+        :collapse="isCollapse"
+        :collapse-transition="false"
         background-color="#304156"
         text-color="#bfcbd9"
         active-text-color="#409EFF"
       >
         <el-menu-item index="/dashboard">
           <el-icon><Monitor /></el-icon>
-          <span>总览</span>
+          <template #title>总览</template>
         </el-menu-item>
         <el-menu-item index="/lvs">
           <el-icon><Connection /></el-icon>
-          <span>LVS管理</span>
+          <template #title>LVS管理</template>
         </el-menu-item>
         <el-menu-item index="/nginx">
           <el-icon><Document /></el-icon>
-          <span>Nginx管理</span>
+          <template #title>Nginx管理</template>
         </el-menu-item>
         <el-menu-item index="/k8s">
           <el-icon><Box /></el-icon>
-          <span>K8S发布</span>
+          <template #title>K8S发布</template>
         </el-menu-item>
         <el-menu-item index="/preprod">
           <el-icon><ZoomOut /></el-icon>
-          <span>预生产扩缩容</span>
+          <template #title>预生产扩缩容</template>
         </el-menu-item>
         <el-menu-item index="/logs">
           <el-icon><List /></el-icon>
-          <span>操作日志</span>
+          <template #title>操作日志</template>
         </el-menu-item>
         <el-menu-item v-if="userStore.isAdmin" index="/servers">
           <el-icon><Setting /></el-icon>
-          <span>服务器管理</span>
+          <template #title>服务器管理</template>
         </el-menu-item>
       </el-menu>
+
+      <div style="position: absolute; bottom: 0; width: 100%; display: flex; justify-content: center; padding: 10px 0; cursor: pointer; color: #bfcbd9;" @click="isCollapse = !isCollapse">
+        <el-icon :size="20"><Fold v-if="!isCollapse" /><Expand v-else /></el-icon>
+      </div>
     </el-aside>
 
     <el-container>
@@ -59,15 +65,16 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useUserStore } from '../stores/user'
 import { getUserInfo } from '../api'
-import { Monitor, Connection, Document, Box, ZoomOut, List, Setting } from '@element-plus/icons-vue'
+import { Monitor, Connection, Document, Box, ZoomOut, List, Setting, Fold, Expand } from '@element-plus/icons-vue'
 
 const route = useRoute()
 const router = useRouter()
 const userStore = useUserStore()
+const isCollapse = ref(false)
 
 onMounted(async () => {
   try {
@@ -83,3 +90,10 @@ function handleLogout() {
   router.push('/login')
 }
 </script>
+
+<style scoped>
+.el-aside {
+  position: relative;
+  overflow: hidden;
+}
+</style>
